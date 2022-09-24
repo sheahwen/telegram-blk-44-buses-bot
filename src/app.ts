04759b479c5a81c1a1bc2ae44c6f273654bccbd5
sheahwen <sheahwen@gmail.com>
2022-09-25 01:04:47 +0800

    Start command works!

--- src/app.ts
+++ src/app.ts
@@ -5,3 +5,10 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN!);
+
+// to start
+bot.start((ctx) => {
+  ctx.reply("Start command is triggered");
+});
+
+bot.launch();
