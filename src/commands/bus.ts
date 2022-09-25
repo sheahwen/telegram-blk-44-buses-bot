918d270afaa39ebdbe0a8541f446ec246ce21a66
sheahwen <sheahwen@gmail.com>
2022-09-25 09:42:17 +0800

    Added text formatter to send arrival times

--- src/commands/bus.ts
+++ src/commands/bus.ts
@@ -1,20 +1,20 @@
 import Context from "telegraf/typings/context";
-import { getWaitingTime } from "../../lib/apiRequest/getWaitingTime";
+import { formatMessage } from "../../lib/controller/formatMessage";
+import { getWaitingTime } from "../../lib/controller/getWaitingTime";
 import { busData } from "../../lib/data/buses";
 
-type Times = Record<string, number[]>;
+export type Times = Record<string, number[]>;
 
 export const busCommand = async (ctx: Context) => {
   // get arrival times for all three buses - 231, 232, 235
   const times: Times = {};
 
   for (const bus of busData) {
-    times.serviceNo = (await getWaitingTime(bus.code, bus.serviceNo)) || [];
+    times[bus.serviceNo] =
+      (await getWaitingTime(bus.code, bus.serviceNo)) || [];
   }
 
-  console.log(times);
-
-  ctx.reply("yes");
+  ctx.reply(formatMessage(times));
 
   // logic to pick buses
 };
