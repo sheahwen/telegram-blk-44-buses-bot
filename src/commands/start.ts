d5e67927bea5a8778b80a2c89b6171913636abf3
sheahwen <sheahwen@gmail.com>
2022-09-25 21:43:42 +0800

    Updated start command

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -2,6 +2,6 @@ import Context from "telegraf/typings/context";
 import { listOfCommandsMsg } from "../../lib/constant/replyMessages";
 
 export const startCommand = (ctx: Context) => {
-  ctx.reply("Hi I am your bus assistant");
+  ctx.reply("Hi lovely housemates of almighty LSW. I am your bus assistant");
   ctx.reply(listOfCommandsMsg);
 };
