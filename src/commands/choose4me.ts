4ffad1178176da58a2155dc8b577846bbe166487
sheahwen <sheahwen@gmail.com>
2022-09-25 22:03:57 +0800

    Added printing of arrival times to choose4me function

--- src/commands/choose4me.ts
+++ src/commands/choose4me.ts
@@ -1,5 +1,11 @@
 import Context from "telegraf/typings/context";
+import { formatArrivalTimes } from "../../lib/controller/formatArrivalTimes";
+import { getListOfArrivalTimes } from "../../lib/controller/getListOfArrivalTimes";
 
 export const choose4MeCommand = async (ctx: Context) => {
   ctx.reply(`Ok! Here you go!`);
+
+  // display all arrival times
+  const times = await getListOfArrivalTimes();
+  ctx.reply(formatArrivalTimes(times));
 };
