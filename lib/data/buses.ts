6e526ca60faa13ae3a1eabc75ebfb93f3f72017a
sheahwen <sheahwen@gmail.com>
2022-09-25 21:27:36 +0800

    Added two more buses to bus data

--- lib/data/buses.ts
+++ lib/data/buses.ts
@@ -1,1 +1,5 @@
-export const busData = [{ code: "52289", serviceNo: "235" }];
+export const busData = [
+  { code: "52289", serviceNo: "235" },
+  { code: "52291", serviceNo: "231" },
+  { code: "52359", serviceNo: "232" },
+];
