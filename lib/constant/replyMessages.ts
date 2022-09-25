a101d4ba643bc39cfb9dd975376dd28a7a544158
sheahwen <sheahwen@gmail.com>
2022-09-25 21:45:44 +0800

    Updated list of commands

--- lib/constant/replyMessages.ts
+++ lib/constant/replyMessages.ts
@@ -2,5 +2,6 @@ export const listOfCommandsMsg = `
 Say something to me
 /start - start the bot
 /bus - get arrival times of nearby bus services
+/choose4me - get the best route via a data driven approach
 /help - command reference
 `;
